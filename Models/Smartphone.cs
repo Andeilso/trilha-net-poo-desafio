1f4bee559c7c8f42c846ea7a64b5663a1ce026da
Andeilso Alves <andeilsoalves@gmail.com>
2023-12-22 12:56:31 -0300

    Desafio concluido!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,23 +4,44 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo;
+        private string Imei;
+        private int Memoria;
+        protected List<string> AppsInstalados;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            AppsInstalados = new List<string>();
         }
+        
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
+            Console.WriteLine();
         }
 
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine();
         }
 
+        public void ListarAppsInstalados()
+        {
+            Console.WriteLine();
+            Console.WriteLine("  --- App's Instalados ---");
+            foreach (string app in AppsInstalados)
+            {
+                Console.WriteLine($"     {app}");
+            }
+            Console.WriteLine("  ------------------------");
+            Console.WriteLine();
+        }
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia MD12 -------------------------------------------------------");
+Console.WriteLine();
+Nokia nokiaMD12 = new Nokia("98754-4893", "MD12", " 564392-58-430065-7", 128);
+nokiaMD12.Ligar();
+nokiaMD12.InstalarAplicativo("WhatsApp");
+nokiaMD12.ListarAppsInstalados();
+Console.WriteLine("------------------------------------------------------------------");
+Console.WriteLine();
+
+
+Console.WriteLine("Nokia MD14 -------------------------------------------------------");
+Console.WriteLine();
+Nokia nokiaMD14 = new Nokia("987544893", "MD14", " 240652-98-198000-7", 256);
+nokiaMD14.ReceberLigacao();
+nokiaMD14.InstalarAplicativo("Instagram");
+nokiaMD14.ListarAppsInstalados();
+Console.WriteLine("------------------------------------------------------------------");
+Console.WriteLine();
+
+
+Console.WriteLine("Iphone 12 Pro ----------------------------------------------------");
+Console.WriteLine();
+Iphone iphone12Pro = new Iphone("98754-4893", "12Pro", " 195165-28-980015-1", 128);
+iphone12Pro.ReceberLigacao();
+iphone12Pro.InstalarAplicativo("Telegram");
+iphone12Pro.InstalarAplicativo("Facebook");
+iphone12Pro.InstalarAplicativo("SnapChat");
+iphone12Pro.InstalarAplicativo("Instagram");
+iphone12Pro.InstalarAplicativo("WhatsApp");
+iphone12Pro.ListarAppsInstalados();
+Console.WriteLine("------------------------------------------------------------------");
+Console.WriteLine();
